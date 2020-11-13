87e8acf75f37e9b3fe5003ffc7e8b292ba239e95
Ni cute <nicute@NideMacBook-Pro.local>
2020-11-13 16:28:23 +0800

    Origin

---
+++ JbpIDFA.h
@@ -0,0 +1,15 @@
+//
+//  JbpIDFA.h
+//  Study
+//
+//  Created by Ni cute on 2020/11/13.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JbpIDFA : NSObject
+
++ (NSString *)getJbpIDFA;
+
+@end
+
